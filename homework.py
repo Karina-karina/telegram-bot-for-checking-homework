5d3c103202a7be68336265f82e9524ec340f70da
imac <imac@iMac-imac.local>
2020-06-26 13:45:04 +0500

    7

--- homework.py
+++ homework.py
@@ -63,7 +63,7 @@ def main():
                     new_homework.get('homeworks')[0]))
             current_timestamp = new_homework.get(
                 'current_date')  # обновить timestamp
-            time.sleep(30 * 60)  # опрашивать раз 30 минут
+            time.sleep(10 * 60)  # опрашивать раз в 10 минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
